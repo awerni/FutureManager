82a147ff83b97cd58a61b42d6596d10c8d43c9b1
Michal Jakubczak <michal.jakubczak@ardigen.com>
2020-07-20 14:18:47 +0200

    Improved unit tests

--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -1,9 +1,9 @@
-runClick <- function(app, id){
+runClick <- function(app, id, cancel = FALSE){
   app$executeScript(sprintf("$('#%s').click();", id))
   app$waitForValue(
     name = id, 
     iotype = "input",
-    ignore = list(FALSE, NULL)
+    ignore = list(cancel, NULL)
   )
 }
 
@@ -81,11 +81,6 @@ test_that(
     
     app$setInputs(xVar = "Petal.Length")
     runClick(app, "plot_run")
-    app$waitFor(
-      expr = "$('#plot').find('img').length == 0;",
-      checkInterval = 500,
-      timeout = 2000 # 2s
-    )
     
     plot <- getValue(app, "plot", FALSE)
     expect_equal(
@@ -93,5 +88,58 @@ test_that(
       expected = "wait for the process"
     )
     expect_true("fm-wait" %in% plot$type)
+    
+    runClick(app, "plot_run", TRUE) # cancel the process
+    
+    app$waitFor( # wait for the cancel
+      expr = "$('#plot').text() === 'run the process first';",
+      checkInterval = 500,
+      timeout = 2000 # 2s
+    )
+    
+    plotBtn <- app$findElement("#plot_run")
+    expect_true("btn-danger" %in% plotBtn$getClass())
+    
+    # expected error in the process (fmError)
+    app$setInputs(xVar = "Species")
+    runClick(app, "plot_run")
+    
+    app$waitFor(
+      expr = "$('#plot').hasClass('shiny-output-error-fm-failed');",
+      checkInterval = 500,
+      timeout = 2000 # 2s
+    )
+    
+    plot <- getValue(app, "plot", FALSE)
+    expect_equal(
+      object = plot$message,
+      expected = "Species column not allowed as xVar"
+    )
+    expect_true("fm-failed" %in% plot$type)
+    
+    plotBtn <- app$findElement("#plot_run")
+    expect_true("btn-danger" %in% plotBtn$getClass())
+    
+    # unexpected error in the process (stop)
+    app$setInputs(xVar = "Sepal.Width")
+    app$setInputs(yVar = "Species")
+    
+    runClick(app, "plot_run")
+    
+    app$waitFor(
+      expr = "! $('#plot').hasClass('shiny-output-error-validation');",
+      checkInterval = 500,
+      timeout = 2000 # 2s
+    )
+
+    plot <- getValue(app, "plot", FALSE)
+    expect_equal(
+      object = plot$message,
+      expected = "Species column not allowed as yVar"
+    )
+    expect_null(plot$type)
+
+    plotBtn <- app$findElement("#plot_run")
+    expect_true("btn-danger" %in% plotBtn$getClass())
   }
 )
